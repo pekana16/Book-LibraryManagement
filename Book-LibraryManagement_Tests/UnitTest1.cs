4dd1e4e709b7864fa15d1578e4e04393af440bf6
Milos Pekic <pekic.milos11@gmail.com>
2025-02-26 15:41:21 +0100

    Managed to create Unit test and write some stuff in there.. everything seems to be working well

---
+++ Book-LibraryManagement_Tests/UnitTest1.cs
@@ -0,0 +1,64 @@
+using Book_LibraryManagement.BusinessLogic;
+
+namespace Book_LibraryManagement_Tests
+{
+    [TestFixture]
+    public class LibraryManagerTests
+    {
+        private LibraryManager _manager;
+
+        [SetUp]
+        public void Setup()
+        {
+            // this is something which runs before each test
+            _manager = new LibraryManager();
+
+        }
+
+        [Test]
+        public void AddNewBook_ShouldIncreaseBookCount()
+        {
+            // Arrange
+            var initialCount = _manager.GetAllBooks().Count;
+
+            // Act
+            _manager.AddNewBook("Test Book", "Test Author");
+            var newCount = _manager.GetAllBooks().Count;
+
+            // Assert
+            Assert.That(newCount, Is.EqualTo(initialCount + 1));
+        }
+
+        [Test]
+        public void GetBookById_ShouldReturnCorrectBook()
+        {
+            // Arrange
+            _manager.AddNewBook("Some Title", "Some Author");
+            var lastBook = _manager.GetAllBooks().LastOrDefault();
+            var lastId = lastBook?.Id ?? -1;
+
+            // Act
+            var foundBook = _manager.GetBookById(lastId);
+
+            // Assert
+            Assert.That(foundBook, Is.Not.Null);
+            Assert.That(foundBook.Title, Is.EqualTo("Some Title"));
+        }
+
+        [Test]
+        public void DeleteBook_ShouldRemoveBookFromCollection()
+        {
+            // Arrange
+            _manager.AddNewBook("Delete Me", "Temp Author");
+            var lastBook = _manager.GetAllBooks().LastOrDefault();
+            var lastId = lastBook?.Id ?? -1;
+
+            // Act
+            _manager.DeleteBook(lastId);
+            var deletedBook = _manager.GetBookById(lastId);
+
+            // Assert
+            Assert.That(deletedBook, Is.Null);
+        }
+    }
+}
